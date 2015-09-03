acaeed3a6e07534b49d0f2550d345c0b5041ad29
jondauz <jondauz@gmail.com>
2015-09-02 22:29:50 -0700

    Merge branch 'master' into gh-pages
    Conflicts:
    	index.html
    	js/geocontent.js

--- README.md
+++ README.md
@@ -1,1 +1,80 @@
-# geocontent
+GeoContent
+============
+
+GeoContent provides the ability to change content depending on location. First, GeoContent uses [icanhazip.com](https://major.io/icanhazip-com-faq/) to retreive the user's IP and then uses [freegeoip](http://freegeoip.net/) to get user's location from the user's IP.
+
+## Installation
+Bower installation 
+```javascript
+bower install geocontent
+```
+GeoContent requires JQuery to be loaded. Include geocontent.js script after loading JQuery.
+```javascript
+<script src="/path/to/jquery.min.js"></script>
+<script src="/path/to/geocontent.js"></script>
+```
+## Setup and Initialization
+Before initilizing the plug-in you need to first set-up the plug-in with the content that needs to be localized. 
+### Setting up 
+Using the `change()` function you'll be able to add elements that need to be localized. 
+```javascript
+GeoContent.change({
+  type: text|image,
+  element: $(".element-to-localize"),
+  content: string|array,
+});
+```
+### Changing Text
+```javascript
+GeoContent.change({
+  type: 'text',
+  element: $(".geo-text"),
+  content: "How's the weather in %%region_name%%?"
+});
+```
+option | Type | Default | Description
+------ | ---- | ------- | -----------
+type (required) | string | null | "text" or "image" are the possible choices. If you wish to localize text you would use "text". If you want to localize an image use "image".
+element (required)|$(element)|null| This is a jQuery selector of the element you wish to localize. 
+content (required)|string|null| When localizing  text using **%%region_name%%** and **%%region_code%%** in the string will be replaces with the user’s location region code or region name. Example: “How’s the weather in %%region_name%%?” will end up reading, “How’s the weather in California?” if you live in California.
+regions (optional) | array |  null | An array of region codes, which is used to specify which "regions" will have their content changed. If no array of regions is given, then the content will change no matter what the region of the user. 
+exclude (optional) | boolean |  false | If exclude is true the regions array will be used as a list of regions to **not** change the content of. 
+
+
+### Changing an Image 
+```javascript
+GeoContent.change({
+  type:'image',
+  element: $('.geo-image'),
+  content: [
+    {
+      region: "CA",
+      image: "images/california-dog.jpg"
+    }
+  ] 
+});
+```
+option | Type | Default | Description
+------ | ---- | ------- | -----------
+type (required) | string | null | "text" or "image" are the possible choices. If you wish to localize text you would use "text". If you want to localize an image use "image".
+element (required)|$(element)|null| This is a jQuery selector of the element you wish to localize. 
+content (required)|array|null| When localizing an image "content" must be an array of objects which contain "region" (string) and "image" (string). "region" represents what region should be shown this image should be shown. "image" represents the path of the new image.
+
+### Initialization
+After you set-up the plug-in you need to initialize the plug-in. 
+```javascript
+GeoContent.init({
+    ip: string,
+    mygeourl: string
+});
+```
+
+option | Type | Default | Description
+------ | ---- | ------- | -----------
+ip (optional) | string | null | Rather then having plug-in retreive your IP, you can pass in an IP instead.
+mygeourl (optional)|string|null| freegeoip provides a way for developers to create there own webserver using the freegeoip code. If you've created your own webserver you can pass the url to the API using this option.
+
+
+
+
+
--- js/geocontent.js
+++ js/geocontent.js
@@ -1,166 +1,192 @@
-;(function($) {
+var GeoContent = (function ($) {
 
+<<<<<<< HEAD
 	// Hide content that will be changed
 <<<<<<< HEAD
 	var geoContentElems = $('[data-geo-content]');
   geoContentElems.css('opacity','0');
 =======
 	var geoContentElems = $('[data-geo-content]').css('opacity','0');
+=======
+  var geoContent = {},
+    getipurl =  "http://icanhazip.com/",
+    location = null,
+    elemsToLocalize = [];
 
-  /// Set up AJAX default values 
+  geoContent.settings = {
+    ip: null,
+    mygeourl: 'https://freegeoip.net/json',
+    onComplete: null,
+  }
+>>>>>>> master
+
   $.ajaxSetup({
+<<<<<<< HEAD
     timeout: 4000
   });
 >>>>>>> master
+=======
+    timeout: 3000
+  }); 
+>>>>>>> master
 
-  // Define our constructor 
-  this.GeoContent = function() {
+  geoContent.change = function(elemObject) {
 
-  	var geoObject = this;
-  	this.geoContent = null;
-
-  	geoObject.getIpUrl = "http://icanhazip.com/";
-
-    // Define option defaults 
-    geoObject.options = {
-      mygeourl: 'https://freegeoip.net/json',
-    	ip: null,
-      exclude: false,
-      regions: []  
-    }
-
-    // Create options by extending defaults with the passed in arugments
-    if (arguments[0] && typeof arguments[0] === "object") {
-      $.extend(geoObject.options, arguments[0]);
-    }
-
-    if(geoObject.options&&(geoObject.options.ip===null||geoObject.options.ip==='')) {
-  	
-    	$.ajax({
-  			url: geoObject.getIpUrl
-  		}).done(function(ip){	
-  		
-  			geoObject.options.ip = ip;
-  			initGeoContent(geoObject);
-  		
-  		}).error(function(){
-
-        console.error('Unable to get IP.')
-        showElements();
-
-      });
-
-  	} else {
-
-   		initGeoContent(geoObject);
-  	
-  	}
+    elemsToLocalize.push(elemObject);
 
   }
 
-  function initGeoContent(geoObject) {
- 
+  geoContent.init = function(options) {
+   
+    $.extend(geoContent.settings, options);
+    
+    if(geoContent.settings.ip) {
+      getLocation();
+    } else {
+      getUserIP();
+    }
+
+  };
+
+  function changeText(elemObject) {
+
+    if(!elemObject.regions) {
+      updateContent(elemObject.element, elemObject.content);
+    }
+    else if(elemObject.regions&&elemObject.regions.indexOf(location.region_code)>=0&&(!elemObject.exclude||elemObject.exclude==false)) {
+      updateContent(elemObject.element, elemObject.content);
+    }
+    else if(elemObject.regions&&elemObject.regions.indexOf(location.region_code)==-1&&elemObject.exclude==true) {
+      updateContent(elemObject.element, elemObject.content);
+    }
+
+    elemObject.element.css('opacity','1');
+
+  }
+
+  function changeImage(elemObject) {
+    
+    if($.isArray(elemObject.content)) {
+       
+       $.each(elemObject.content, function(index, value){
+      
+        if(value.region===location.region_code) {
+          elemObject.element.attr('src',value.image);
+          elemObject.element.load(function(){
+            elemObject.element.css('opacity','1');
+          });
+          return;
+        }
+
+        if(elemObject.content.length-1===index) {
+          elemObject.element.css('opacity','1');        
+        }
+
+      });
+
+    } else {
+      showError('"content" must be an array for type image');
+    }
+
+  }  
+
+  function updateContent(elem, template) {
+    
+    var newText = "";
+    // Change Region Code
+    if(template.indexOf('%%region_code%%')>0&&location.region_code!=='') {
+      newText = template.replace('%%region_code%%', location.region_code);
+    }
+    // Change Region Name
+    else if(template.indexOf('%%region_name%%')>0&&location.region_name!=='') {
+      newText = template.replace('%%region_name%%', location.region_name);
+    } 
+    
+    if(newText!=='') {
+      elem.text(newText);
+    }
+   
+  }
+
+  function getUserIP() {
+
     $.ajax({
-			url: geoObject.options.mygeourl + '/' + geoObject.options.ip
-		}).done(function(result){	
-		  
-      geoObject.geoContent = result;
+      url: getipurl
+    }).done(function(ip){ 
 
-      if(geoObject.options.exclude) {
-        updateExcludedContent(geoObject);
-      } else {
-        updateContent(geoObject);
-      }
-
-		}).error(function(error){
+      geoContent.settings.ip = ip;
+      getLocation();
      
-      console.error('Unable to get geolocate.')
+     }).error(function(){
+
+      showError('Unable to get IP.');
       showElements();
     
     });
- 
+
   }
 
-  function updateExcludedContent(geoObject) {
+  function getLocation() {
 
-    if(geoObject.options.regions.length==0) {
-      
+    $.ajax({
+      url: geoContent.settings.mygeourl + '/' + geoContent.settings.ip      
+    }).done(function(userlocation){ 
+
+      location = userlocation;
+      changeContent();
+
+    }).error(function(error){
+     
+      showError('Unable to get geolocate.');
       showElements();
-      console.error("Array of regions to exclude can't be empty!");
     
-    } else {
+    });
 
-      if(geoObject.options.regions.indexOf(geoObject.geoContent.region_code)<0) {
+  }
 
-        $.each(geoContentElems, function(index, value){
+  function changeContent() {
+    
+    $.each(elemsToLocalize, function(index, value){
+
+      if(!value.type) {
+
+        showError('type is required for' + value.element.selector.toString());
       
-          var current = $(value),
-            currentData = $(value).data('geo-content'),
-            newText = '';
-          
-          // Change Region Code
-          if(currentData.indexOf('%%region_code%%')>0&&geoObject.geoContent.region_code!=='') {
-            newText = currentData.replace('%%region_code%%', geoObject.geoContent.region_code);
-          }
-          // Chnage Region Name
-          else if(currentData.indexOf('%%region_name%%')>0&&geoObject.geoContent.region_name!=='') {
-            newText = currentData.replace('%%region_name%%', geoObject.geoContent.region_name);
-          } 
-          
-          if(newText!=='') {
-            current.text(newText);
-          }
-          current.css('opacity','1');
-
-        });
-        
       } else {
-        
-        showElements();
-      
+
+        if(value.content) {
+           if(value.type=="text") {
+            changeText(value);
+          }
+          else if(value.type=="image") {
+            changeImage(value);
+          }
+        } else {
+          showError('content is required for ' + value.element.selector.toString());
+        }
+
       }
-    
-    }    
-  
-  }
+     
+    });
 
-  function updateContent(geoObject) {
-    
-    if(geoObject.options.regions.length==0||geoObject.options.regions.indexOf(geoObject.geoContent.region_code)>=0) {
-
-      $.each(geoContentElems, function(index, value){
-    
-        var current = $(value),
-          currentData = $(value).data('geo-content'),
-          newText = '';
-        
-        // Change Region Code
-        if(currentData.indexOf('%%region_code%%')>0&&geoObject.geoContent.region_code!=='') {
-          newText = currentData.replace('%%region_code%%', geoObject.geoContent.region_code);
-        }
-        // Chnage Region Name
-        else if(currentData.indexOf('%%region_name%%')>0&&geoObject.geoContent.region_name!=='') {
-          newText = currentData.replace('%%region_name%%', geoObject.geoContent.region_name);
-        } 
-        
-        if(newText!=='') {
-          current.text(newText);
-        }
-        current.css('opacity','1');
-
-      });
-      
-    } else {
-      showElements();
-    }
-
-  	
   }
 
   function showElements() {
-  
-    geoContentElems.css('opacity','1');
+
+    $.each(elemsToLocalize, function(index, value){
+      
+      value.element.css('opacity','1');
+
+    });
 
   }
+
+  function showError(msg) {
+
+    console.error(msg);
+
+  }
+
+  return geoContent;
 
 }(jQuery));
